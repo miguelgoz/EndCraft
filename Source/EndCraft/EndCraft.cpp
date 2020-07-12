b27d45e4f856e6397d78cae8317eda11bdd1ccf2
Miguel Angel Beltrán Rodríguez <68168635+miguelgoz@users.noreply.github.com>
2020-07-11 19:14:30 -0500

    init commit

---
+++ Source/EndCraft/EndCraft.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EndCraft.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EndCraft, "EndCraft" );
---
+++ Source/EndCraft/EndCraftGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EndCraftGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENDCRAFT_API AEndCraftGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
